1199a44e318d27f85c32416e9562fbd5d2eeb86b
煖風遲來 <zjy2872786663@126.com>
2024-11-07 00:05:04 +0800

    网络框架：玩家，房间管理，房间搜索

--- Assets/Scripts/Network/NetworkDiscovery.cs
+++ Assets/Scripts/Network/NetworkDiscovery.cs
@@ -5,6 +5,17 @@ namespace Network
 {
     public class NetworkDiscovery : NetworkDiscoveryBase<ClientRequest, ServerResponse>
     {
+        public static NetworkDiscovery instance;
+
+        private void Awake()
+        {
+            if(instance != null)
+            {
+                DestroyImmediate(this.gameObject);
+            }
+            instance = this;
+        }
+
         protected override ServerResponse ProcessRequest(ClientRequest request, IPEndPoint endpoint)
         {
             return new ServerResponse
--- Assets/Scripts/Network/NetworkRoomManager.cs
+++ Assets/Scripts/Network/NetworkRoomManager.cs
@@ -2,6 +2,16 @@ namespace Network
 {
     public class NetworkRoomManager : Mirror.NetworkRoomManager
     {
+        public static NetworkRoomManager instance;
 
+        private new void Awake()
+        {
+            base.Awake();
+            if (instance != null)
+            {
+                DestroyImmediate(this.gameObject);
+            }
+            instance = this;
+        }
     }
 }
