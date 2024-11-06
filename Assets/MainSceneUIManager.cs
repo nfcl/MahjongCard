07b2426e84d6aebabb95a632aaf38c115e22d6e5
煖風遲來 <zjy2872786663@126.com>
2024-11-06 22:38:55 +0800

    添加项目文件。

---
+++ Assets/MainSceneUIManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MainSceneUI
+{
+    public class MainSceneUIManager : MonoBehaviour
+    {
+        public static MainSceneUIManager instance;
+
+        public LoginPanel   loginPanel;     //登录界面
+        public LobbyPanel   lobbyPanel;     //大厅界面
+        public SearchPanel  searchPanel;    //搜索界面
+        public RoomPanel    roomPanel;      //房间界面
+
+        private void Awake()
+        {
+            instance = this;
+        }
+    }
+}
---
+++ Assets/SearchPanel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace MainSceneUI
+{
+    public class SearchPanel : MonoBehaviour
+    {
+
+    }
+}
