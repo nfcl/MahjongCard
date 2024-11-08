39fb14bb0f35bcfeab02d6bfc06ecccaf466982b
煖風遲來 <zjy2872786663@126.com>
2024-11-08 11:13:51 +0800

    UserInfo序列化添加

--- Assets/Scripts/Data/UserInfo.cs
+++ Assets/Scripts/Data/UserInfo.cs
@@ -1,3 +1,6 @@
+using Data;
+using Mirror;
+
 namespace Data
 {
     public class UserInfo
@@ -5,5 +8,24 @@ namespace Data
         public int id;
         public string name;
         public string uuid;
+    }
+}
+
+public static class UserInfoSerializer
+{
+    public static void Writer(this NetworkWriter writer, UserInfo value)
+    {
+        writer.WriteInt(value.id);
+        writer.WriteString(value.name);
+        writer.WriteString(value.uuid);
+    }
+    public static UserInfo Reader(this NetworkReader reader)
+    {
+        return new UserInfo
+        {
+            id = reader.ReadInt(),
+            name = reader.ReadString(),
+            uuid = reader.ReadString()
+        };
     }
 }
